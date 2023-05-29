a0dcf488381506fb492865418993c7a113e4744a
Mukul Borkar (NCS) <mukul.borkar@in.ncs-i.com>
2023-05-29 16:36:15 +0530

    fixed sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,49 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Sidebar.css";
-import { getCurrentUser } from "../auth";
+import { getCurrentUser, getToken } from "../auth";
 import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Sidebar: React.FC = () => {
   const userData = getCurrentUser();
-  const image = "http://localhost:8081/api/users/image/" + userData.id;
+  const token = getToken();
+  const image =
+    getCurrentUser.image == null
+      ? window.location.origin + "/default.png"
+      : "http://localhost:8081/api/users/image/" + userData.id;
   console.log(userData);
   const [profileImage, setProfileImage] = useState(image);
 
-  const handleImageUpload = () => {};
+  const handleImageUpload = async () => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      userData.imageUrl = file.name;
+      console.log(file.name);
+      setProfileImage(file.name);
+    };
+    reader.readAsDataURL(file);
+    const image = new FormData();
+    image.append("profileImage", file);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8081/api/users/image/upload/" + userData.id,
+        image,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      toast.success("Image Uploaded");
+      console.log(response.data);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-      <div className="sidebar position-static">
+      <div className="sidebar">
         <form>
           <div className="content">
             <label htmlFor="profileImage">
@@ -33,7 +63,7 @@ const Sidebar: React.FC = () => {
             />
           </div>
         </form>
-        <div>
+        <div className="user-data">
           <h4 style={{ color: "white", textAlign: "center" }}>
             {userData.name}
           </h4>
